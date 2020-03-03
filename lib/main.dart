3d0926f0ae1b4fb13bbe75c7fa057a6025cd5858
Innocent Dema <31923567+angwandi@users.noreply.github.com>
2020-03-04 02:10:02 +0300

    Connect app to firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
+import 'package:demschat/screens/chat_screen.dart';
+import 'package:demschat/screens/login_screen.dart';
+import 'package:demschat/screens/registration_screen.dart';
 import 'package:demschat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(FlashChat());
+/*void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CustomSplash(
+        imagePath: 'images/logo.png',
+        backGroundColor: Colors.blueGrey,
+        animationEffect: 'zoom-in',
+        logoSize: 200,
+        home: FlashChat(),
+        duration: 2500,
+        type: CustomSplashType.StaticDuration,
+      ),
+    ));*/
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
 
 class ChatScreen extends StatefulWidget {
+  static const String id = 'chat_screen';
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser loggedInUser;
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+    print(loggedInUser.email);
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
